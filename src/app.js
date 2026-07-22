00f44fae002c7ae61a1ab66107c3e2f5230753b8
teybearr <altheaquiatchon214@gmail.com>
2026-07-22 15:26:31 +0800

    fix: displays in app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,30 @@ import { fetchSampleUsers } from './api.js';
 
 // console.log('Server starting...');
 
-// console.log(formatDate(new Date("2026-07-22")));
-// console.log(validateTask());
-// console.log(mergeTaskUpdate({title:"Old"}, {title:"New"}));
+console.log(formatDate(new Date("2026-07-22")));
+console.log(validateTask());
+console.log(mergeTaskUpdate({title:"Old"}, {title:"New"}));
 
 async function fsu() {
-  const users = await fetchSampleUsers();
-  console.log('Result:', users);
+    const users = await fetchSampleUsers();
+    console.log('Result:', users);
 }
 fsu();
+
+try {
+    const validTaskData = { title: 'submit itelect gt4', dueDate: '2026-07-22', };
+
+    const newTask = createTask(validTaskData);
+    console.log('Task created:', newTask);
+} catch (error) {
+    console.error(' Unexpected error:', error.message);
+}
+
+try {
+    const invalidTaskData = { title: 'missing Due Date', };
+
+    const invalidTask = createTask(invalidTaskData);
+    console.log('Task created:', invalidTask);
+} catch (error) {
+    console.error(' Unexpected error:', error.message);
+}
